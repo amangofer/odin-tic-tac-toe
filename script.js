70bc0b8f5ccd45a4f7e6c0ba49c74ce50a81aec0
Amanuel Haile <amanuelhaile369@gmail.com>
2023-07-16 15:12:48 +0300

    create a function that render the gameboard
      - create a function displayController that render the game controls
      - create score variable on player function
      - create getScore and updateScore to to get and update the score

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 
-const gameBoard = (() =>{
+const gameBoard = (() => {
     const board =[
-        "","","",
-        "","","",
-        "","",""
+        "x","o","o",
+        "x","x","o",
+        "x","o","o"
     ];
 
     const getGameBoard = () => {
         return board;
     }
 
-    return {getGameBoard};
+    const updateGameBoard = (index, marker) => {
+        board[index] = marker;
+    }
+
+    return {getGameBoard, updateGameBoard};
 })();
 
 function player(name, marker) {
-    return {name, marker};
+    let score = 0;
+
+    const getScore = () => {
+        return score;
+    }
+    const updateScore = () => {
+        score += 1;
+    }
+    return {name, marker, getScore, updateScore};
 }
 
+const displayController = ((arr) => {
+
+    const main = document.querySelector('main');
+
+    const gameboardGrid = document.createElement('div');
+    gameboardGrid.className = "game-board";
+    arr.forEach(element => {
+        const box = document.createElement('div');
+        box.className = "box";
+        const boxMarker = document.createElement('div');
+        boxMarker.className = element == "x" ? "x-color" : "o-class";
+        boxMarker.appendChild(document.createTextNode(element));
+        box.appendChild(boxMarker);
+        gameboardGrid.appendChild(box);
+    });
+    
+    const restart = document.createElement('button');
+    restart.appendChild(document.createTextNode("Restart the game"));
+    restart.id = "restart";
+
+    main.appendChild(gameboardGrid);
+    main.appendChild(restart);
+
+    
+})(gameBoard.getGameBoard());
